3ddc39fbef26b02cd3b3bee21443764ef60e97eb
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-03 15:34:56 -0400

    imported react-router-dom as dependency, cleared css files, added businessCard component, added navbar component, added userProfile component

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BusinessCard from "../components/BusinessCard";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>This is the homepage</h1>
+      <div>
+        <BusinessCard />
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
