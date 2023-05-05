8d6ac2180e8b5175fbbb9a645750f181ec1069b4
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-04 20:21:53 -0400

    got query for businessCard to work

--- client/src/components/BusinessCard.jsx
+++ client/src/components/BusinessCard.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { QUERY_BUSINESSES } from "../utils/queries";
+import { useQuery } from "@apollo/client";
 
-const BusinessCard = ({ businesses }) => {
+const BusinessCard = () => {
+  const { data } = useQuery(QUERY_BUSINESSES);
+  const businesses = data?.businesses || [];
+  console.log("data in businessCard", data);
+
   return (
     <div className="business-cards">
-      {businesses.map((business) => (
-        <div className="business-card">
+      {businesses.map((business, i) => (
+        <div key={i} className="business-card">
           <div className="business-header">
             <h2>{business.name}</h2>
           </div>
           <div className="business-content">
-            <img src={business.imageUrl} alt="business-profile-pic" />
+            <img src={business.sponsor} alt="business-profile-pic" />
             <p>{business.description}</p>
           </div>
           <button type="button">Click to Launch</button>
