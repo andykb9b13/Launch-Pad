3e02105c947296d7c8a51200378eb579f5e888d3
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-05-04 21:41:46 -0400

    fixed tailwind

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/launchpad-logo-placeholder.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
-    <div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#A3DE83] text-white">
+      <div>
+        <img src={Logo} alt='LaunchPad Logo' style={{ width: '50px' }} />
+      </div>
+      
+      
       <h1>This is the navbar</h1>
       <Link to="/">Home</Link>
       <Link to="/business">Business</Link>
