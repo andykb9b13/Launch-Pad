16649440f76b5957b53dd6bccbad76441046b0b9
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-09 22:21:18 -0400

    added icons for social media sites on businessCards

--- client/src/components/BusinessCard/BusinessCard.jsx
+++ client/src/components/BusinessCard/BusinessCard.jsx
@@ -3,6 +3,9 @@ import { QUERY_BUSINESSES } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 import Products from "../Products";
+import twitter from "../../assets/icons/twitter.png";
+import facebook from "../../assets/icons/facebook.png";
+import instagram from "../../assets/icons/instagram.png";
 import "./style.css";
 
 const BusinessCard = () => {
@@ -27,6 +30,9 @@ const BusinessCard = () => {
               <h5 className="mb-2 text-xl font-bold leading-tight">
                 {business.name}
               </h5>
+              <p>
+                <a href={business.website}>{business.website}</a>
+              </p>
               <p className="mb-4">{business.location}</p>
               <p className="mb-4">{business.description}</p>
               <p className="mb-4">{business.missionStatement}</p>
@@ -38,18 +44,22 @@ const BusinessCard = () => {
               >
                 <Link to={`/business/${business.name}`}>Launch Me</Link>
               </button>
+
               <p className="mb-4 text-center text-base font-medium text-sm relative flex flex-row">
-                <span className="hover:scale-150 duration-200 relative top-5 left-0 basis-1/4">
-                  <a href={business.website}>Company Website</a>
+                <span className="hover:scale-150 duration-200 relative top-5 basis-1/4">
+                  <a href={business.facebook}>
+                    <img src={facebook} alt="" />
+                  </a>
                 </span>
                 <span className="hover:scale-150 duration-200 relative top-5 basis-1/4">
-                  <a href={business.facebook}>Facebook</a>
+                  <a href={business.twitter}>
+                    <img src={twitter} alt="" />
+                  </a>
                 </span>
                 <span className="hover:scale-150 duration-200 relative top-5 basis-1/4">
-                  <a href={business.twitter}>Twitter</a>
-                </span>
-                <span className="hover:scale-150 duration-200 relative top-5 basis-1/4">
-                  <a href={business.instagram}>Instagram</a>
+                  <a href={business.instagram}>
+                    <img src={instagram} alt="" />
+                  </a>
                 </span>
               </p>
             </div>
