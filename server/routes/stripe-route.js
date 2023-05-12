e7503c58015cdc28abc05084277399cb70ebc536
jmckelvey786 <77301869+jmckelvey786@users.noreply.github.com>
2023-05-11 20:11:27 -0400

    added route folder, updated server.js, added some dependencies

---
+++ server/routes/stripe-route.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+
+const secretKeyFromStripe = "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";
+//we need secret key from stripe here \/
+const stripe = require("stripe")(secretKeyFromStripe);
+const { v4:uuidv4 } = require('uuid');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    console.log("Stripe Route Researcher")
+    res.json ({
+        message: 'It Works!'
+    })
+});
+
+router.post("/pay", (req, res, next)=> {
+    console.log(req.body.token);
+    const {token, amount} = req.body;
+    const idempotencyKey = uuidv4();
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token
+    }).then(customer => {
+        stripe.charges.create({
+            amount: amount * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email
+        }, {idempotencyKey})
+    }).then(result => {
+        res.status(200).json(result)
+    }).catch(err=>{
+        console.log(err);
+    });
+});
+
+
+module.exports = router;
