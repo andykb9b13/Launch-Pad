501723211a067180b86a677323bdd0dbca46a591
jmckelvey786 <77301869+jmckelvey786@users.noreply.github.com>
2023-05-13 23:29:29 -0400

    more styling

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Auth from '../utils/auth';
 import { useMutation } from "@apollo/react-hooks";
 import { ADD_USER } from "../utils/mutations";
+import "../styles/login.css"
 
 function SignUp() {
   const [formData, setFormData] = useState({
@@ -99,34 +100,35 @@ function SignUp() {
 
   return (
     <div className='w-full h-screen flex justify-center items-center p-4'>
-      <form className='border-4 border-[var(--green)] rounded-lg flex flex-col max-w-[800px] w-full bg-[var(--white)] p-6' onSubmit={handleSubmit}>
+      {/* <form className='login-border rounded-lg flex flex-col max-w-[800px] w-full bg-[var(--white)] p-6' onSubmit={handleSubmit}> */}
+      <form className='login-border padding-20 top-margin fade-in-box' onSubmit={handleSubmit}>
         <label className='text-[var(--red)] tracking-wider sm:text-2xl'> 
-          Username : 
-          <input placeholder=' username' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)] ml-2' type="text" name="username" value={formData.username} onChange={handleChange} />
+          Username: 
+          <br /><input placeholder=' username' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)]' type="text" name="username" value={formData.username} onChange={handleChange} />
           {formErrors.username && <span className="error">{formErrors.username}</span>}
         </label>
         <br />
         <label className='text-[var(--red)] tracking-wider sm:text-2xl'>
-          Email :
-          <input placeholder=' email' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)] ml-2' type="email" name="email" value={formData.email} onChange={handleChange} />
+          Email:
+          <br /><input placeholder=' email' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)]' type="email" name="email" value={formData.email} onChange={handleChange} />
           {formErrors.email && <span className="error">{formErrors.email}</span>}
         </label>
         <br />
         <label className='text-[var(--red)] tracking-wider sm:text-2xl'>
-          Password :
-          <input placeholder=' password' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)] ml-2' type="password" name="password" value={formData.password} onChange={handleChange} />
+          Password:
+          <br /><input placeholder=' password' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)]' type="password" name="password" value={formData.password} onChange={handleChange} />
           {formErrors.password && <span className="error">{formErrors.password}</span>}
         </label>
         <br />
         <label className='text-[var(--red)] tracking-wider sm:text-2xl'>
-          Confirm Password :
-          <input placeholder=' password' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)] ml-2' type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+          Confirm Password:
+          <br /><input placeholder=' password' className='bg-[var(--white)] my-2 text-[gray] p-2 border-2 rounded-lg border-[var(--green)]' type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
           {formErrors.confirmPassword && <span className="error">{formErrors.confirmPassword}</span>}
         </label>
         <br />
-        <div className='flex'>
-        <button className='bg-[var(--white)] border-2 border-[var(--green)] rounded-lg hover:bg-[var(--lime)] hover:text-[var(--white)] px-10 py-3 my-2 mx-auto flex flex-center' type="submit">Sign Up</button>
-        <button className='bg-[var(--white)] border-2 border-[var(--green)] rounded-lg hover:bg-[var(--red)] hover:text-[var(--white)] px-10 py-3 my-2 mx-auto flex flex-center' type="button" onClick={ handleClear }>Cancel</button>
+        <div className='flex space-evenly gap-10'>
+        <button className='button-background button-background:hover border-2 border-[var(--green)] rounded-lg hover:text-[var(--white)] px-10 py-3 my-2 mx-auto flex flex-center' type="submit">Sign Up</button>
+        <button className='button-background button-background:hover border-2 border-[var(--green)] rounded-lg hover:text-[var(--white)] px-10 py-3 my-2 mx-auto flex flex-center' type="button" onClick={ handleClear }>Cancel</button>
         </div>
       </form>
     </div>
