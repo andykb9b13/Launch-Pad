5f12a314376ceef82baa8dd6c1ee5315d87b5df0
jmckelvey786 <77301869+jmckelvey786@users.noreply.github.com>
2023-05-14 20:11:56 -0400

    changed styling on products

--- client/src/pages/BusinessProfile.jsx
+++ client/src/pages/BusinessProfile.jsx
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import Products from "../components/Products";
 import AddProduct from "../components/AddProduct";
+import "../styles/business.css"
 
 const BusinessProfile = () => {
   const { name } = useParams();
@@ -25,16 +26,19 @@ const BusinessProfile = () => {
 
   return (
     <div>
-      <div>
-        <h2>{business.name}</h2>
-      </div>
-      <h2>These are the items that need funding</h2>
+      <div className="business-flex-page">
+        <div>
+          <h2>{business.name}</h2>
+        </div>
+      <h2>These are the items that need Some funding</h2>
       <Products products={business.products} />
       <div>
         <h2>Add a product for funding</h2>
 
-        <AddProduct business={business} />
+          <AddProduct business={business} />
+        </div>
       </div>
+      
     </div>
   );
 };
